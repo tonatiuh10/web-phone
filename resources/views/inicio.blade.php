b8e86a41bbc61f736357e2a4f64a08f148da05ef
Josue <josue102299@gmail.com>
2025-02-16 15:38:59 -0600

    Nueva linea en la vista de inicio

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-{{auth()->user()->id}}
+<p>User> </p>{{auth()->user()->id}}
 <form action="{{route('logout')}}" method='GET'>
     <button type='submit'>Cerrar Sesion</button>
 </form>
